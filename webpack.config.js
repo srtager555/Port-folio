92fc77d411be7570f5656b39ebdf1238dba7946f
Carlos && Ponce <srtager555@gmail.com>
2022-01-22 00:00:30 -0600

    webpack devServer configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const parh = require("path");
+const path = require("path");
 
 module.exports = {
    output: {
       filename: "[name].bundle.js",
-      path: parh.resolve(__dirname, "build"),
+      path: path.resolve(__dirname, "build"),
    },
    plugins: [
       new MiniCssExtractPlugin(),
       new HtmlWebpackPlugin({ template: "./public/index.html" }),
    ],
+   devServer: {
+      static: {
+         directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 9000,
+   },
    module: {
       rules: [
          {
-            test: [/\.m?js$/, /\.jsx$/],
+            test: [/\.m?js$/, /\.jsx?$/],
             exclude: /(node_modules|bower_components)/,
             use: {
                loader: "babel-loader",
