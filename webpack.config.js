b3ead9ae84dc13af6cb61c0fdab37a17079eca01
Carlos && Ponce <srtager555@gmail.com>
2022-01-21 23:28:17 -0600

    Plugins for  webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+   plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({ template: "./public/index.html" }),
+   ],
+   module: {
+      rules: [
+         {
+            test: [/\.m?js$/, /\.jsx$/],
+            exclude: /(node_modules|bower_components)/,
+            use: {
+               loader: "babel-loader",
+               options: {
+                  presets: ["@babel/preset-env", "@babel/preset-react"],
+               },
+            },
+         },
+         {
+            test: [/\.css$/],
+            use: [MiniCssExtractPlugin.loader, "css-loader"],
+         },
+      ],
+   },
+};
