9dd539741ebce4dac2b766eb4fd13bee79f0adab
Carlos && Ponce <srtager555@gmail.com>
2022-02-20 00:48:27 -0600

    navbar first part ended

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,30 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "../../styles/Navbar.css";
+
 export function Navbar() {
 
     return (
-        <nav className="nav__container">
+        <>
             <div className="nav__background"></div>
-            <div className="nav__logo">
-                <a href="/">
-                    <img width={50} src="https://ttager.netlify.app/img/logo-new-top.png" alt="logo" />
-                </a>
-            </div>
-            <div className="nav__content">
-                <div className="nav__links">
-                    <a href="#home">Home</a>
-                    <a href="#projects">Projects</a>
-                    <a href="#about-me">About Me</a>
-                    <a href="#lest-talk">Lest Talk</a>
-                    <Link to="a">-a</Link>
+            <nav className="nav__container">
+                <div className="nav__logo">
+                    <a href="/">
+                        &&
+                    </a>
                 </div>
-                <div className="equalizer">
-                    <div className="equalizer__bar"></div>
-                    <div className="equalizer__bar"></div>
-                    <div className="equalizer__bar"></div>
-                </div>
-            </div>
-        </nav>
+                {/* <div className="nav__content"> */}
+                    <div className="nav__links">
+                        <a href="#home">Home</a>
+                        <a href="#projects">Projects</a>
+                        <a href="#about-me">About Me</a>
+                        <a href="#lest-talk">Lest Talk!</a>
+                    </div>
+                    <div className="equalizer">
+                        <div className="equalizer__bar"></div>
+                        <div className="equalizer__bar"></div>
+                        <div className="equalizer__bar"></div>
+                    </div>
+                {/* </div> */}
+            </nav>
+        </>
     )
 }
