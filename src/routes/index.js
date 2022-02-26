c84d79658012a15514174f605a324f630d910233
Carlos && Ponce <srtager555@gmail.com>
2022-02-26 16:41:36 -0600

    temporal not macth and "cannt get /" solved

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,15 @@ import { HomePage } from "../pages/home";
 import { Navbar } from "../components/navbar";
 import { Background } from "../components/background";
 
-export function Router() {
+function NotMatch() {
+    return (
+        <div>
+            <h1>404</h1>
+        </div>
+    )
+}
 
+export function Router() {
     return (
         <>
             <Background />
@@ -15,7 +22,7 @@ export function Router() {
             <div className="container-content">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="*" element={<div>404</div>} />
+                    <Route path="*" element={<NotMatch />} />
                 </Routes>
             </div>
         </>
