a42b218eaa1d091c326b32655d5ad048259178e3
Carlos && Ponce <srtager555@gmail.com>
2022-02-26 15:42:14 -0600

    Element component of project ready

---
+++ src/components/projectElement/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { ProjectsElementList, recentProjects } from "../../contexts/projectsContexts";
+
+export function ProjectsElement({ SliderProjectsRef, ViewProjectsScroll, ProjectsElementRef }) {
+
+    return (
+        <div ref={SliderProjectsRef} style={ViewProjectsScroll} className="container__projects-scrollSlider-item">
+            {
+                recentProjects.map((element, index) => {
+                    return (
+                        (index > 1) ?
+                            <div key={`project-${index}`} className="container__image-projects__scrollSlider">
+                                <img src={element.PrincipalImageMobile} alt="kda Ahri" />
+                            </div> :
+                            <div key={
+                                `project-${index}`
+                            } ref={ProjectsElementRef} className="container__image-projects__scrollSlider">
+                                <img src={element.PrincipalImageMobile} alt="kda Ahri" />
+                            </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
