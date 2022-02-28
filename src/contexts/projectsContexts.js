42665d7ec074f69799eaa5b542f0862c4a92911f
Carlos && Ponce <srtager555@gmail.com>
2022-02-27 21:30:11 -0600

    New titles added and solved a problem with recentProjects

--- src/contexts/projectsContexts.js
+++ src/contexts/projectsContexts.js
@@ -1,6 +1,7 @@
 export function ProjectElement({
     Title = "",
     Description = "",
+    Id = "",
 
     PrincipalImageDesktop = "",
     PrincipalImageMobile = "",
@@ -13,6 +14,7 @@ export function ProjectElement({
 }) {
     this.Title = Title;
     this.Description = Description;
+    this.Id = Id;
 
     this.PrincipalImageDesktop = PrincipalImageDesktop;
     this.PrincipalImageMobile = PrincipalImageMobile;
@@ -30,6 +32,7 @@ export function ProjectsElementList() {
         new ProjectElement({
             Title: " Title",
             Description: " Description",
+            Id: "p00",
             PrincipalImageDesktop: "",
             PrincipalImageMobile: "https://i.ibb.co/2Zcw0X4/flor.jpg",
             BackgroundImageDesktop: "",
@@ -44,40 +47,9 @@ export function ProjectsElementList() {
             Date: "2020-01-01",
         }),
         new ProjectElement({
-            Title: " Title",
+            Title: "Wrapping Letters",
             Description: " Description",
-            PrincipalImageDesktop: "",
-            PrincipalImageMobile: "https://i.ibb.co/2Zcw0X4/flor.jpg",
-            BackgroundImageDesktop: "",
-            BackgroundImageMobile: "",
-            GaleryImages: [
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            ],
-            Link: "https://www.google.com",
-            Date: "2020-01-01",
-        }),
-        new ProjectElement({
-            Title: " Title",
-            Description: " Description",
-            PrincipalImageDesktop: "",
-            PrincipalImageMobile: "https://i.ibb.co/2Zcw0X4/flor.jpg",
-            BackgroundImageDesktop: "",
-            BackgroundImageMobile: "",
-            GaleryImages: [
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-                "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-            ],
-            Link: "https://www.google.com",
-            Date: "2020-01-01",
-        }),
-        new ProjectElement({
-            Title: " Title",
-            Description: " Description",
+            Id: "p01",
             PrincipalImageDesktop: "",
             PrincipalImageMobile: "https://i.ibb.co/rHtcHGY/bosque.jpg",
             BackgroundImageDesktop: "",
@@ -92,8 +64,9 @@ export function ProjectsElementList() {
             Date: "2020-01-01",
         }),
         new ProjectElement({
-            Title: " Title",
+            Title: "Wrapping Letters HomePage",
             Description: " Description",
+            Id: "p02",
             PrincipalImageDesktop: "",
             PrincipalImageMobile: "https://i.ibb.co/b2JvX8R/coven-2x.jpg",
             BackgroundImageDesktop: "",
@@ -108,8 +81,9 @@ export function ProjectsElementList() {
             Date: "2020-01-01",
         }),
         new ProjectElement({
-            Title: " Title",
+            Title: "Rynyk",
             Description: " Description",
+            Id: "p03",
             PrincipalImageDesktop: "",
             PrincipalImageMobile: "https://i.ibb.co/X5Wg7S5/kda.jpg",
             BackgroundImageDesktop: "",
@@ -124,8 +98,9 @@ export function ProjectsElementList() {
             Date: "2020-01-01",
         }),
         new ProjectElement({
-            Title: " Title",
+            Title: "Port-folio",
             Description: " Description",
+            Id: "p04",
             PrincipalImageDesktop: "",
             PrincipalImageMobile: "https://i.ibb.co/2Zcw0X4/flor.jpg",
             BackgroundImageDesktop: "",
@@ -144,4 +119,4 @@ export function ProjectsElementList() {
 }
 
 const ProjectList = ProjectsElementList()
-export const recentProjects = ProjectList.slice(ProjectList.length - 5, ProjectList.length - 1);
+export const recentProjects = ProjectList.slice(ProjectList.length - 4, ProjectList.length);
