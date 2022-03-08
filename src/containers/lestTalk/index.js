df10972c8cb6517c2a5f3d7636ba8ffb0af19f7b
Carlos && Ponce <srtager555@gmail.com>
2022-03-08 03:53:09 -0600

    hover animations with animjs

--- src/containers/lestTalk/index.js
+++ src/containers/lestTalk/index.js
@@ -1,45 +1,140 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import WrappingLetters from "wrapping-letters-react";
+import anime from "animejs";
 
-export function LestTalkContainer({ l }) {
+import "../../styles/LestTalk.css";
+
+export function LestTalkContainer() {
+
+    const linkedinRef = useRef(null);
+    const githubRef = useRef(null);
+    const behanceRef = useRef(null);
+
+    function moveUpLetterFirstChild() {
+        anime
+            .timeline({
+                duration: 500,
+                easing: "easeInOutSine",
+            })
+            .add({
+                targets: linkedinRef.current.children[0].childNodes,
+                translateY: [0, "-200%"],
+                delay: anime.stagger(20),
+            })
+            .add({
+                targets: linkedinRef.current.children[1].childNodes,
+                translateY: ["200%", 0],
+                delay: anime.stagger(20),
+            }, 0)
+    }
+    function moveUpLetterSecondChild() {
+        anime
+            .timeline({
+                duration: 500,
+                easing: "easeInOutSine",
+            })
+            .add({
+                targets: linkedinRef.current.children[0].childNodes,
+                translateY: ["-200%", 0],
+                delay: anime.stagger(20),
+            })
+            .add({
+                targets: linkedinRef.current.children[1].childNodes,
+                translateY: [0, "200%"],
+                delay: anime.stagger(20),
+            }, 0)
+    }
+
+    useEffect(() => {
+        moveUpLetterFirstChild()
+        setTimeout(() => {
+            moveUpLetterSecondChild()
+        }, 1000)
+        // linkedinRef.current.addEventListener("mouseenter", () => );
+    }, []);
+
+
     return (
-        <div ref={l} id="lest-talk" className="container container_lestTalk">
+        <div id="lest-talk" className="container container_lestTalk">
             <div className="button-mail">
                 <a href="mailto:srtager555@gmail.com">
-                    <WrappingLetters
-                        word="Let's Talk"
-                        wordOptions={[{
-                            ClassToAdd: "button-mail-letter",
-                        }]}
-                    />
+                    <div className="container__text">
+                        <WrappingLetters
+                            word="Let's Talk"
+                            wordOptions={[{
+                                ClassToAdd: "button-mail-letter",
+                            }]}
+                        />
+                    </div>
+                    <div className="container__text">
+                        <WrappingLetters
+                            word="Let's Talk"
+                            wordOptions={[{
+                                ClassToAdd: "button-mail-letter",
+                            }]}
+                        />
+                    </div>
                 </a>
             </div>
             <div className="container__social_networks">
-                <a href="https://www.linkedin.com/in/srtager555/" target="_blank" rel="noopener noreferrer">
-                    <WrappingLetters
-                        word="LinkedIn"
-                        wordOptions={[{
-                            ClassToAdd: "social-network-letter",
-                        }]}
-                    />
-                </a>
-                <a href="https://www.github.com/srtager555" target="_blank" rel="noopener noreferrer">
-                    <WrappingLetters
-                        word="Github"
-                        wordOptions={[{
-                            ClassToAdd: "social-network-letter",
-                        }]}
-                    />
-                </a>
+                <h2>About me</h2>
+                <div className="container_networks">
+                    <a ref={linkedinRef} onMouseEnter={() => console.log(linkedinRef.current.children[0].children)} href="https://www.linkedin.com/in/srtager555/" target="_blank" rel="noopener noreferrer">
+                        <div className="container-text">
+                            <WrappingLetters
+                                word="LinkedIn"
+                                wordOptions={[{
+                                    ClassToAdd: "social-network-letter",
+                                }]}
+                            />
+                        </div>
+                        <div className="container-text">
+                            <WrappingLetters
+                                word="LinkedIn"
+                                wordOptions={[{
+                                    ClassToAdd: "social-network-letter",
+                                }]}
+                            />
+                        </div>
+                    </a>
+                    <a ref={githubRef} href="https://www.github.com/srtager555" target="_blank" rel="noopener noreferrer">
+                        <div className="container-text">
+                            <WrappingLetters
+                                word="Github"
+                                wordOptions={[{
+                                    ClassToAdd: "social-network-letter",
+                                }]}
+                            />
+                        </div>
+                        <div className="container-text">
+                            <WrappingLetters
+                                word="Github"
+                                wordOptions={[{
+                                    ClassToAdd: "social-network-letter",
+                                }]}
+                            />
+                        </div>
 
-                <a href="https://www.behance.net/srtager555" target="_blank" rel="noopener noreferrer">
-                    <WrappingLetters
-                        word="Behance"
-                        wordOptions={[{
-                            ClassToAdd: "social-network-letter",
-                        }]}
-                    />
-                </a>
+                    </a>
+                    <a ref={behanceRef} href="https://www.behance.net/srtager555" target="_blank" rel="noopener noreferrer">
+                        <div className="container-text">
+                            <WrappingLetters
+                                word="Behance"
+                                wordOptions={[{
+                                    ClassToAdd: "social-network-letter",
+                                }]}
+                            />
+                        </div>
+                        <div className="container-text">
+                            <WrappingLetters
+                                word="Behance"
+                                wordOptions={[{
+                                    ClassToAdd: "social-network-letter",
+                                }]}
+                            />
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     );
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,14 +3,14 @@ import React from "react";
 import { HomeContainer } from "../../containers/home";
 import { ProjectsContainer } from "../../containers/projects";
 import { LestTalkContainer } from "../../containers/lestTalk";
-import { AboutMeContainer } from "../../containers/aboutMe";
+// import { AboutMeContainer } from "../../containers/aboutMe";
 
 export function HomePage() {
     return (
         <>
             <HomeContainer />
             <ProjectsContainer />
-            <AboutMeContainer />
+            {/* <AboutMeContainer /> */}
             <LestTalkContainer  />
         </>
     );
