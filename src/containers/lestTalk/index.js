31be1367048f545481238e3e8fb51dfabcb65462
Carlos && Ponce <srtager555@gmail.com>
2022-03-09 18:04:18 -0600

    lestTalk css and anime ready

--- src/containers/lestTalk/index.js
+++ src/containers/lestTalk/index.js
@@ -6,81 +6,91 @@ import "../../styles/LestTalk.css";
 
 export function LestTalkContainer() {
 
+    const lestTalkRef = useRef(null);
     const linkedinRef = useRef(null);
     const githubRef = useRef(null);
     const behanceRef = useRef(null);
 
-    function moveUpLetterFirstChild() {
+    function moveUpLetterAnimation2(element) {
+        console.log(element)
+        anime
+            .timeline({
+                targets: element.current.children[0].childNodes,
+                duration: 1000,
+                easing: "easeInOutSine",
+            })
+            .add({
+                translateY: [0, "-100%", 0],
+                delay: anime.stagger(30),
+            })
+    }
+    function moveUpLetterAnimation(element) {
         anime
             .timeline({
                 duration: 500,
                 easing: "easeInOutSine",
             })
             .add({
-                targets: linkedinRef.current.children[0].childNodes,
+                targets: element.current.children[0].childNodes,
                 translateY: [0, "-200%"],
                 delay: anime.stagger(20),
             })
             .add({
-                targets: linkedinRef.current.children[1].childNodes,
+                targets: element.current.children[1].childNodes,
                 translateY: ["200%", 0],
                 delay: anime.stagger(20),
             }, 0)
     }
-    function moveUpLetterSecondChild() {
+    function moveDownLetterAnimation(element) {
         anime
             .timeline({
                 duration: 500,
                 easing: "easeInOutSine",
             })
             .add({
-                targets: linkedinRef.current.children[0].childNodes,
+                targets: element.current.children[0].childNodes,
                 translateY: ["-200%", 0],
                 delay: anime.stagger(20),
             })
             .add({
-                targets: linkedinRef.current.children[1].childNodes,
+                targets: element.current.children[1].childNodes,
                 translateY: [0, "200%"],
                 delay: anime.stagger(20),
             }, 0)
     }
 
-    useEffect(() => {
-        moveUpLetterFirstChild()
-        setTimeout(() => {
-            moveUpLetterSecondChild()
-        }, 1000)
-        // linkedinRef.current.addEventListener("mouseenter", () => );
-    }, []);
 
 
     return (
         <div id="lest-talk" className="container container_lestTalk">
             <div className="button-mail">
-                <a href="mailto:srtager555@gmail.com">
+                <a ref={lestTalkRef} onMouseEnter={() => moveUpLetterAnimation2(lestTalkRef)} href="mailto:srtager555@gmail.com">
                     <div className="container__text">
                         <WrappingLetters
-                            word="Let's Talk"
+                            word="Let's Talk !!"
                             wordOptions={[{
                                 ClassToAdd: "button-mail-letter",
-                            }]}
-                        />
-                    </div>
-                    <div className="container__text">
-                        <WrappingLetters
-                            word="Let's Talk"
-                            wordOptions={[{
-                                ClassToAdd: "button-mail-letter",
+                                SelectClass: {
+                                    wordToSearch: "!!",
+                                    classToAdd: "button-mail-letter_exclamation",
+                                }
                             }]}
                         />
                     </div>
                 </a>
+                <div className="border-letstalk"></div>
             </div>
             <div className="container__social_networks">
                 <h2>About me</h2>
                 <div className="container_networks">
-                    <a ref={linkedinRef} onMouseEnter={() => console.log(linkedinRef.current.children[0].children)} href="https://www.linkedin.com/in/srtager555/" target="_blank" rel="noopener noreferrer">
-                        <div className="container-text">
+                    <a
+                        ref={linkedinRef}
+                        onMouseEnter={() => moveUpLetterAnimation(linkedinRef)}
+                        onMouseLeave={() => moveDownLetterAnimation(linkedinRef)}
+                        href="https://www.linkedin.com/in/srtager555/"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        <div className="container__text">
                             <WrappingLetters
                                 word="LinkedIn"
                                 wordOptions={[{
@@ -88,7 +98,7 @@ export function LestTalkContainer() {
                                 }]}
                             />
                         </div>
-                        <div className="container-text">
+                        <div className="container__text">
                             <WrappingLetters
                                 word="LinkedIn"
                                 wordOptions={[{
@@ -97,8 +107,15 @@ export function LestTalkContainer() {
                             />
                         </div>
                     </a>
-                    <a ref={githubRef} href="https://www.github.com/srtager555" target="_blank" rel="noopener noreferrer">
-                        <div className="container-text">
+                    <a
+                        ref={githubRef}
+                        onMouseEnter={() => moveUpLetterAnimation(githubRef)}
+                        onMouseLeave={() => moveDownLetterAnimation(githubRef)}
+                        href="https://www.github.com/srtager555"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <div className="container__text">
                             <WrappingLetters
                                 word="Github"
                                 wordOptions={[{
@@ -106,7 +123,7 @@ export function LestTalkContainer() {
                                 }]}
                             />
                         </div>
-                        <div className="container-text">
+                        <div className="container__text">
                             <WrappingLetters
                                 word="Github"
                                 wordOptions={[{
@@ -116,8 +133,15 @@ export function LestTalkContainer() {
                         </div>
 
                     </a>
-                    <a ref={behanceRef} href="https://www.behance.net/srtager555" target="_blank" rel="noopener noreferrer">
-                        <div className="container-text">
+                    <a
+                        ref={behanceRef}
+                        onMouseEnter={() => moveUpLetterAnimation(behanceRef)}
+                        onMouseLeave={() => moveDownLetterAnimation(behanceRef)}
+                        href="https://www.behance.net/srtager555"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <div className="container__text">
                             <WrappingLetters
                                 word="Behance"
                                 wordOptions={[{
@@ -125,7 +149,7 @@ export function LestTalkContainer() {
                                 }]}
                             />
                         </div>
-                        <div className="container-text">
+                        <div className="container__text">
                             <WrappingLetters
                                 word="Behance"
                                 wordOptions={[{
