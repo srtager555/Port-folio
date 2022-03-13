d7b75d56cf4ad2862fcbd30231c0cdae885aad1c
Carlos && Ponce <srtager555@gmail.com>
2022-03-12 21:47:27 -0600

    Map about to present the content of project

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,9 +2,33 @@ import React from "react";
 import WrappingLetters from "wrapping-letters-react";
 import { useParams } from "react-router-dom";
 
-import { ProjectsElementList } from "../../contexts/projectsContexts";
+import {
+   ProjectsElementList,
+   ImageWithText,
+} from "../../contexts/projectsContexts";
 
 import "../../styles/ProjectPage.css";
+
+function ImageProcess({ project, index }) {
+    return  project.GaleryImages.map((image) => {
+        if (typeof image === "string") {
+           return <img key={`image ${index}`} className="project-image" src={image} alt="" />;
+        } else {
+           if (!image instanceof ImageProcess) {
+              throw new Error(
+                 "ImageProcess: image must be a string or an ImageProcess"
+              );
+           }
+           return <div key={`image ${index}`} className="project__image-container">
+                 <div className="project__image-container__text">
+                     <h3 className="project__image-container__text__title">{image.Title}</h3>
+                     <p className="project__image-container__text__description">{image.Description}</p>
+                 </div>
+                 <img className="project__image" src={image.Image} alt="" />
+           </div>;
+        }
+    });
+}
 
 export function ProjectPage() {
    //get information from the path of the url and put it in the variable
@@ -36,6 +60,9 @@ export function ProjectPage() {
                <img src={project.PrincipalImageMobile} alt={project.Title} />
             </div>
          </header>
+         <div className="project__content">
+                  <ImageProcess project={project} />
+         </div>
       </div>
    );
 }
