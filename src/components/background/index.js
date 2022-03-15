04b1c7a856419bd3b02419220728892a5c4f848b
Carlos && Ponce <srtager555@gmail.com>
2022-03-15 00:15:27 -0600

    I tried...

--- src/components/background/index.js
+++ src/components/background/index.js
@@ -1,9 +1,23 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import "../../styles/Background.css";
 
 export function Background() {
-    return (
-        <div className="container container-background"></div>
-    )
+   const [backgroundClass, setBackgroundClass] = useState("");
+
+   useEffect(() => {
+      // XDXDXDXD
+      setInterval(function() {
+         if (window.location.pathname === "/") {
+            setBackgroundClass("");
+         } else {
+            setBackgroundClass("background--project");
+         }
+      }, 1000)
+   });
+
+   return (
+      <div
+         className={`container container-background ${backgroundClass}`}
+      ></div>
+   );
 }
