ad3867e738934beb0d42f56caa8394b0f2469985
Carlos && Ponce <srtager555@gmail.com>
2022-03-15 00:23:05 -0600

    function to reset the scroll

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import { HomePage } from "../pages/home";
@@ -8,26 +8,38 @@ import { Navbar } from "../components/navbar";
 import { Background } from "../components/background";
 
 function NotMatch() {
-    return (
-        <div>
-            <h1>404</h1>
-        </div>
-    )
+   return (
+      <div>
+         <h1>404</h1>
+      </div>
+   );
 }
 
 export function Router() {
-    return (
-        <>
-            <Background />
-            <Navbar />
-            <div className="container-content">
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/:id" element={<ProjectPage />} />
-                    <Route path="*" element={<NotMatch />} />
-                </Routes>
-            </div>
-        </>
+   function handleRouteChange() {
+      window.scrollTo(0, 0);
+   }
 
-    )
+   useEffect(() => {
+      handleRouteChange();
+   })
+
+   return (
+      <>
+         <Navbar />
+         <Background />
+         <div className="container-content">
+            <Routes>
+               <Route path="/" element={<HomePage />} />
+               <Route
+                  path="/:id"
+                  element={
+                     <ProjectPage handleRouteChange={handleRouteChange} />
+                  }
+               />
+               <Route path="*" element={<NotMatch />} />
+            </Routes>
+         </div>
+      </>
+   );
 }
