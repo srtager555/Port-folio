6e5033dbf69e00d377ee4e5cf21829c561ea89ac
Carlos && Ponce <srtager555@gmail.com>
2022-03-17 03:14:52 -0600

    ?

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       static: {
          directory: path.join(__dirname, "public/"),
       },
-      
+
       compress: true,
       port: 9000,
       historyApiFallback: true,
