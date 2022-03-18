3dd6e3f0aa4c725296bec3daf340501bdd234553
Carlos && Ponce <srtager555@gmail.com>
2022-03-17 20:47:40 -0600

    I got and put the nw to place the special class.

--- src/components/projectImage/index.js
+++ src/components/projectImage/index.js
@@ -6,77 +6,79 @@ import { ImageWithText } from "../../contexts/projectsContexts";
 
 export function ImageProcess({ project, containerWitdh }) {
    const [imageWidth, setImageWidth] = useState([]);
-   const [isLoading, setIsLoading] = useState(false);
-
-   /*
-    Hay que ejecutar el llamado de las imagenes solo una vez, puede ser con un useEffect que
-    llame a un function que haga ese trabajo y que retorne la infomacion dentro de un array,
-    Luego usar la informacion para verificar que clases llevara el componente, esto debe
-    ser dentro de un useState para que haya una actualizacion en el componente.
-   */
+   const [isLoading, setIsLoading] = useState(true);
 
    useEffect(() => {
       let arrayOfPromise = project.GaleryImages.map((image, index) => {
          let i = new Image();
          i.src = typeof image === "string" ? image : image.Image;
          return new Promise((resolve, reject) => {
-            fetch(i.src).then((response) => {
-               if (response.ok)
-                  i.onload = (aaa) => {
-                     console.log(aaa);
-                     let height = i.naturalHeight;
-                     let width = i.naturalWidth;
-                     resolve([width, height, index]);
-                  };
-               else reject("XD");
-            });
+            i.onload = () => {
+               let width = i.naturalWidth;
+               resolve([width, index]);
+            };
          });
       });
 
       Promise.all(arrayOfPromise)
          .then((value) => {
-            console.log(value);
             setImageWidth(value);
+            setIsLoading(false);
          })
          .catch((err) => {
             console.log(err);
          });
    }, []);
 
-   return project.GaleryImages.map((image, index) => {
-      if (typeof image === "string") {
-         return (
-            <div className="project__image">
-               <img
+   const a = isLoading ? (
+      <span>Xd</span>
+   ) : (
+      project.GaleryImages.map((image, index) => {
+         if (typeof image === "string") {
+            return (
+               <div className="project__image">
+                  <img
+                     key={`image ${index}`}
+                     className="project__image-img"
+                     src={image}
+                     alt=""
+                  />
+               </div>
+            );
+         } else {
+            if (!image instanceof ImageWithText) {
+               throw new Error(
+                  "ImageWithText: image must be a string or an ImageWithText"
+               );
+            }
+
+            let imageW = imageWidth.find((a) => a[1] === index);
+            let imageWrap = imageW[0] <= containerWitdh * 0.6 ? true : false;
+
+            return (
+               <div
                   key={`image ${index}`}
-                  className="project__image-img"
-                  src={image}
-                  alt=""
-               />
-            </div>
-         );
-      } else {
-         if (!image instanceof ImageWithText) {
-            throw new Error(
-               "ImageWithText: image must be a string or an ImageWithText"
+                  className={`project__image project__image-container ${
+                     imageWrap ? "project__image-container--not_wrapp" : ""
+                  }`}
+               >
+                  <div className="project__image-container__text">
+                     <h3 className="project__image-container__text-title">
+                        {image.Title}
+                     </h3>
+                     <p className="project__image-container__text-description">
+                        {image.Description}
+                     </p>
+                  </div>
+                  <img
+                     className="project__image-img"
+                     src={image.Image}
+                     alt=""
+                  />
+               </div>
             );
          }
-         return (
-            <div
-               key={`image ${index}`}
-               className="project__image project__image-container"
-            >
-               <div className="project__image-container__text">
-                  <h3 className="project__image-container__text-title">
-                     {image.Title}
-                  </h3>
-                  <p className="project__image-container__text-description">
-                     {image.Description}
-                  </p>
-               </div>
-               <img className="project__image-img" src={image.Image} alt="" />
-            </div>
-         );
-      }
-   });
+      })
+   );
+   return a;
 }
