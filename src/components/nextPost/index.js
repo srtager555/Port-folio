cd5bfc4d5823a337cbf58092e9ca0cfcad0c4b9f
Carlos && Ponce <srtager555@gmail.com>
2022-03-22 03:50:49 -0600

    I finished the logic for progress bar or so I hope XD

--- src/components/nextPost/index.js
+++ src/components/nextPost/index.js
@@ -7,38 +7,42 @@ import { IS_MOBILE_DEVICE } from "../../contexts/constVarible";
 
 export function NextPost({ data, setLoader }) {
    let dataIndex = ProjectsElementList.getProjectIndexAvailableById(data.Id);
-   const navigate = useNavigate()
+   const navigate = useNavigate();
 
    function changeNextRoute() {
-      setLoader(false)
-      setTimeout(()=>{
-         navigate(`/p/${data.Id}`)
-      }, 1000)
+      setLoader(false);
+      setTimeout(() => {
+         navigate(`/p/${data.Id}`);
+      }, 1000);
    }
-   
+
    return (
-      <div onClick={changeNextRoute} className="nextPost__container">
-         <div className="nextPost__background">
-            <img
-               src={
-                  IS_MOBILE_DEVICE
-                     ? data.BackgroundImageMobile
-                     : data.BackgroundImageDesktop
-               }
-            />
-            <div className="nextPost__container--nextWord">
-               <span>NEXT</span>
-               <span>
-                  {dataIndex < 10 ? `0${dataIndex + 1}` : dataIndex + 1}
+      <div className="container__nextPost--main">
+         <div onClick={changeNextRoute} className="nextPost__container">
+            <div className="nextPost__background">
+               <img
+                  src={
+                     IS_MOBILE_DEVICE
+                        ? data.BackgroundImageMobile
+                        : data.BackgroundImageDesktop
+                  }
+               />
+               <div className="nextPost__container--nextWord">
+                  <span>NEXT</span>
+                  <span>
+                     {dataIndex < 10 ? `0${dataIndex + 1}` : dataIndex + 1}
+                  </span>
+               </div>
+            </div>
+            <div className="nextPost__container--info">
+               <h1 className="nextPost__container--info__title">
+                  {data.Title}
+               </h1>
+               <div className="nextPost__container--info__margin__bar"></div>
+               <span className="nextPost__container--info__typeJob">
+                  {data.Job[0]}
                </span>
             </div>
-         </div>
-         <div className="nextPost__container--info">
-            <h1 className="nextPost__container--info__title">{data.Title}</h1>
-            <div className="nextPost__container--info__margin__bar"></div>
-            <span className="nextPost__container--info__typeJob">
-               {data.Job[0]}
-            </span>
          </div>
       </div>
    );
