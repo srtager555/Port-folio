d92dcf65713da6fa37339f8bc79f4326c2f247a7
Carlos && Ponce <srtager555@gmail.com>
2022-05-11 19:35:26 -0600

    cv structure and scss initializated.

--- src/template/cv/index.js
+++ src/template/cv/index.js
@@ -3,6 +3,9 @@ import React from "react";
 import styles from "../../styles/cv.module.scss";
 
 export function CV() {
+   const IMG__PROFILE =
+      "https://i.ibb.co/8KCs9Mw/AEE83958-944-D-4587-83-F2-993-F55-FE25-EF.jpg";
+
    return (
       <div className={styles["container--cv"]}>
          <div
@@ -13,24 +16,31 @@ export function CV() {
             <span>Carlos Ponce</span>
          </div>
          <div
-            className={`${styles.container__content} ${styles["container--profile"]}`}
+            className={`${styles.container__content} ${styles['container--info']}`}
          >
             <h2>Profile</h2>
-            <div className={styles.profileInfo}>
-               <div className={styles["profileInfo--imgBox"]}>
-                  <img
-                     src="https://i.ibb.co/vBK7p1q/profile-pic.jpg"
-                     alt="Hu Tao"
-                  />
-               </div>
-               <div className={styles["profileInfo--infoBox"]}>
-                  <a
-                     href="mailto:srtager555@gmail.com"
-                    //  target="_blank"
-                     rel="noopener noreferrer"
-                  >
-                     srtager555@gmail.com
-                  </a>
+            <div
+               className={`${styles.container__content} ${styles['container--info__profile']}`}
+            >
+               <div className={styles["profile__content"]}>
+                  <div className={styles["profile__Info"]}>
+                     <div className={styles["profile__Info--imgBox"]}>
+                        <img src={IMG__PROFILE} alt="Este soy yo" />
+                     </div>
+                     <div className={styles["profile__Info--infoBox"]}>
+                        <span>Carlos Ponce</span>
+                        <a href="mailto:srtager555@gmail.com">
+                           srtager555@gmail.com
+                        </a>
+                     </div>
+                  </div>
+                  <div className={styles["profile__about"]}>
+                     <p>
+                        I am a Frontend developer with a background in
+                        multimedia and web development. I have a strong passion
+                        for creating and developing web applications.
+                     </p>
+                  </div>
                </div>
             </div>
          </div>
