b8af89902c99ebb6bccde3bf24835b8118fb07a2
Carlos && Ponce <srtager555@gmail.com>
2022-05-14 20:34:43 -0600

    Info changed

--- src/components/cvComponents/containerProfile/index.js
+++ src/components/cvComponents/containerProfile/index.js
@@ -5,6 +5,8 @@ import WrappingLetters from "wrapping-letters-react";
 import styles from "Styles/cv.module.scss";
 
 export function ContainerProfile() {
+   const [year, setYear] = React.useState(new Date().getFullYear());
+
    const IMG__PROFILE =
       "https://i.ibb.co/8KCs9Mw/AEE83958-944-D-4587-83-F2-993-F55-FE25-EF.jpg";
 
@@ -42,9 +44,32 @@ export function ContainerProfile() {
                      </div>
                   </div>
                   <p>
-                     I am a Frontend developer with a background in multimedia
-                     and web development. I have a b passion for creating and
-                     developing web applications.
+                     {/* Soy un joven Frontend developer con experiencia en React
+                     con sus frameworks y librerias. Comodo trabajando en
+                     equipo, trayendo ideas a la realidad con soluciones
+                     practicas y creatividas. */}
+                     I am a young Frontend developer with experience in React
+                     with its frameworks and libraries. comfortable working in
+                     team, bringing ideas to reality with practical and creative
+                     solutions.
+                  </p>
+                  <br />
+                  <p>
+                     {/* Busco un puesto como enmaquetador del front-end en react
+                     como un lider o comepañero de trabajo. */}
+                     I am looking for a position as a front-end packager at
+                     react as a lead or co-worker.
+                  </p>
+                  <br />
+                  <p>
+                     {/* Tengo una base de {`${year - 2019}`} años en desarrollo
+                     Frontend, he vivido una gran cantidad de problemas que he
+                     solucionado, ademas que recientemente he certificcado mis
+                     concimientos ¿que seran unos cuantos problemas mas? */}
+                     I have a base of 3 years in Frontend development, I have
+                     experienced a large number of problems that I have solved,
+                     and I have recently certified my knowledge, what will be a
+                     few more problems?
                   </p>
                   <div className="container__slider cv">
                      <span className="container__slider-item">
