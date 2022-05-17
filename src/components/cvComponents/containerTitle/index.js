86318caf1b23e75ffc6f6bb758b5e7c4f00842af
Carlos && Ponce <srtager555@gmail.com>
2022-05-16 21:45:45 -0600

    Styles for tablets and large mobile added

--- src/components/cvComponents/containerTitle/index.js
+++ src/components/cvComponents/containerTitle/index.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import styles from "../../../styles/cv.module.scss";
+import styles from "Styles/cv.module.scss";
 
 export function ContainerTitle() {
-    return (
-        <div
-        className={`${styles.container__content}  ${styles["container--title"]}`}
-     >
-        <img src="https://i.ibb.co/f8RVct5/home-image.jpg" alt="Hu Tao" />
-        <h1>Frontend developer</h1>
-        <span>Carlos Ponce</span>
-     </div>
-    )
+   const [isMobile, setIsMobile] = useState(false);
+
+   const handleIsMobile = () => {
+      if (window.innerWidth > 510) {
+         setIsMobile(false);
+      } else {
+         setIsMobile(true);
+      }
+   };
+   useEffect(() => {
+      handleIsMobile();
+      window.addEventListener("resize", handleIsMobile);
+      return () => {
+         window.removeEventListener("resize", handleIsMobile);
+      };
+   }, []);
+   return (
+      <div
+         className={`${styles.container__content}  ${styles["container--title"]}`}
+      >
+         {isMobile ? (
+            <img
+               src="https://i.ibb.co/YDnX5r8/mobile-home-image.jpg"
+               alt="Hu Tao"
+            />
+         ) : (
+            <img src="https://i.ibb.co/f8RVct5/home-image.jpg" alt="Hu Tao" />
+         )}
+         <h1>Frontend developer</h1>
+         <span>Carlos Ponce</span>
+      </div>
+   );
 }
