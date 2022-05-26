d1c05a80204b715305f678118669461bc8316a8d
Carlos && Ponce <srtager555@gmail.com>
2022-05-26 01:57:53 -0600

    initializated navbar logic

--- src/template/cv/components/navbar/index.js
+++ src/template/cv/components/navbar/index.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 export function navbar({ pageName, setPageName, pagesName }) {
-  return (
-    <div>navbar</div>
-  )
+   const [buttons, setButtons] = useState([]);
+   const pageIndex = pagesName.indexOf(pageName);
+
+   function buttonComponent(text) {
+      return <button>{text}</button>;
+   }
+
+   useEffect(() => {
+      if (pageIndex === 0) {
+         setButtons([
+            buttonComponent(pagesName[pagesName.length - 1]),
+            buttonComponent(pagesName[pageIndex + 1]),
+         ]);
+      } else if (pageIndex === pagesName.length - 1) {
+         setButtons([
+            buttonComponent(pagesName[pageIndex - 1]),
+            buttonComponent(pagesName[0]),
+         ]);
+      } else {
+         setButtons([
+            buttonComponent(pagesName[pageIndex - 1]),
+            buttonComponent(pagesName[pageIndex + 1]),
+         ]);
+      }
+   }, [pageIndex]);
+
+   return (
+      <>
+         <button>{pagesName[pageIndex - 1]}</button>
+         <button>{pagesName[pageIndex + 1]}</button>
+      </>
+   );
 }
