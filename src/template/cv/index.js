38ef7db943ac3748980fef3b8ddb3684aabc3f6e
Carlos && Ponce <srtager555@gmail.com>
2022-05-26 01:26:33 -0600

    The base of the logic is ready

--- src/template/cv/index.js
+++ src/template/cv/index.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { home } from "./pages/home";
+import { profile } from "./pages/profile";
+import { sq } from "./pages/sq";
+import { experience } from "./pages/experience";
+import { moreInfo } from "./pages/moreInfo";
 
 export function cvtemplate() {
-    return(
-        <div>
-            <h1>CV</h1>
-        </div>
-    )
-} 
+   const [page, setPage] = useState(null);
+   const [pageName, setPageName] = useState("home");
+
+   const pages = [home, profile, sq, experience, moreInfo];
+
+   const pagesName = pages.map((element) => {
+      return element.name;
+   });
+
+   const navbarProps = {
+        pageName,
+        setPageName,
+        pagesName,
+   }
+
+   useEffect(() => {
+      setPage(
+         pages.find((element) => {
+            return element.name === pageName;
+         })
+      );
+   }, [pageName]);
+
+   return <>{page}</>;
+}
