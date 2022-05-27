98ee4f7ad73e07556f05281514b4c7caa0c78ca5
Carlos && Ponce <srtager555@gmail.com>
2022-05-26 18:45:09 -0600

    logic about ht e change the page in the nav is ready

--- src/template/cv/components/navbar/index.js
+++ src/template/cv/components/navbar/index.js
@@ -3,21 +3,26 @@ import React, { useState, useEffect } from "react";
 import { menuButton as MenuButton } from "./menuButton";
 
 export function navbar({ pageName, setPageName, pagesName }) {
+   const [showButton, setShowButton] = useState(false);
    const [buttons, setButtons] = useState([]);
    const pageIndex = pagesName.indexOf(pageName);
 
-    const meuButtonProps = {
-        pageName, handleChangePage, pagesName
-    }
+   const meuButtonProps = {
+      pageName,
+      handleChangePage,
+      pagesName,
+   };
 
-   function handleChangePage(text){ setPageName(text); };
+   function handleChangePage(text) {
+      setPageName(text);
+   }
 
    function buttonComponent(text) {
       return <button onClick={() => handleChangePage(text)}>{text}</button>;
    }
 
    useEffect(() => {
-       //here change the sections to go, depending on the pageName
+      //here change the sections to go, depending on the pageName
       let btn = (prop) => buttonComponent(pagesName[prop]);
       if (pageIndex === 0) {
          setButtons([btn(pagesName.length - 1), btn(pageIndex + 1)]);
@@ -28,10 +33,17 @@ export function navbar({ pageName, setPageName, pagesName }) {
       }
    }, [pageIndex]);
 
+   useEffect(() => {
+      setTimeout(() => {
+         if (pageName === "home") setShowButton(false);
+         else setShowButton(true);
+      }, 1000);
+   }, [pageName]);
+
    return (
       <>
-        <MenuButton {...meuButtonProps} />
-         {buttons.map((element, index) => (
+         <MenuButton {...meuButtonProps} />
+         {showButton && buttons.map((element, index) => (
             <span key={`button-${index}`}>{element}</span>
          ))}
       </>
