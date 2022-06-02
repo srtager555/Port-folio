191edab23c7445f5fc6502931ed514a549049e8a
Carlos && Ponce <srtager555@gmail.com>
2022-06-02 01:16:04 -0600

    orb structures is ready

--- src/template/cv/pages/home/index.js
+++ src/template/cv/pages/home/index.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
-import styles from 'Src/styles/cvStyles/home.module.sass'
+import styles from "Src/styles/cvStyles/home.module.sass";
 
 export function home({ setPageName }) {
-  return (
-    <div>home</div>
-  )
+   return (
+      <div className={styles.home}>
+         <div className={styles.orbLager}>
+            <div angle="0" className={styles.orb}></div>
+            <div angle="15" className={styles.orb}></div>
+            <div angle="30" className={styles.orb}></div>
+            <div angle="45" className={styles.orb}></div>
+            <div angle="60" className={styles.orb}></div>
+            <div angle="75" className={styles.orb}></div>
+         </div>
+         <div className={styles.orbMedium}>
+            <div angle="0" className={styles.orb}></div>
+            <div angle="15" className={styles.orb}></div>
+            <div angle="30" className={styles.orb}></div>
+            <div angle="45" className={styles.orb}></div>
+            <div angle="60" className={styles.orb}></div>
+            <div angle="75" className={styles.orb}></div>
+         </div>
+         <div className={styles.orbLogo}>
+            <span className={styles.logo}>&&</span>
+            <div className={styles.orbBg}>
+               <div angle="0" className={styles.orb}></div>
+               <div angle="15" className={styles.orb}></div>
+               <div angle="30" className={styles.orb}></div>
+               <div angle="45" className={styles.orb}></div>
+               <div angle="60" className={styles.orb}></div>
+               <div angle="75" className={styles.orb}></div>
+            </div>
+         </div>
+      </div>
+   );
 }
