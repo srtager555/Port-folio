42caf9942b84053361c4d26742dab56e9d1e959c
Carlos && Ponce <srtager555@gmail.com>
2022-06-07 16:40:56 -0600

    structure from the main navbar changed

--- src/components/navbar/components/navMain/index.js
+++ src/components/navbar/components/navMain/index.js
@@ -6,23 +6,23 @@ import "../../../../styles/Navbar.css";
 export function NavMain({ handleResetScroll }) {
    return (
       <>
-         <div className="nav__background__container">
+         {/* <div className="nav__background__container"> */}
             <div className="nav__background--main"></div>
-            <div className="nav__background--main"></div>
-         </div>
-         <nav className="nav__container">
-            <div className="nav__container--main">
+            <div className="nav__background--main secondary"></div>
+         {/* </div> */}
+         {/* <nav className="nav__container"> */}
+            <nav className="nav__container--main">
                <div className="nav__logo">
                   <Link onClick={handleResetScroll} to="/">
                      &&
                   </Link>
                </div>
 
-            </div>
-            <div className="nav__container--cta">
+            </nav>
+            <nav className="nav__container--cta">
                <a href="mailto:srtager555@gmail.com">let's talk!</a>
-            </div>
-         </nav>
+            </nav>
+         {/* </nav> */}
       </>
    );
 }
