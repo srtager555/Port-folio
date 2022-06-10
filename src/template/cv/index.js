d2c2522fb5e20ede03ea3fb79d20c8c79a562a30
Carlos && Ponce <srtager555@gmail.com>
2022-06-09 22:52:38 -0600

    new structure for the render about the page is ready

--- src/template/cv/index.js
+++ src/template/cv/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-import { navbar as Navbar } from "./components/navbar";
+// import { navbar as Navbar } from "./components/navbar";
 import { home } from "./pages/home";
 import { profile } from "./pages/profile";
 import { skills_and_qualities as sq } from "./pages/sq";
@@ -11,6 +11,7 @@ import { more_info as moreInfo } from "./pages/moreInfo";
 export function cvtemplate() {
    // url values
    // home, profile, sq, experience, moreInfo
+   const [currentPage, setCurrentPage] = useState(home);
    let { cvId } = useParams();
    const PAGES = [home, profile, sq, experience, moreInfo];
    let functionsName = {
@@ -20,34 +21,37 @@ export function cvtemplate() {
       [experience.name]: experience.name,
       [moreInfo.name]: moreInfo.name,
    };
-   let fnNameKeys = Object.entries(functionsName);
 
-   let pagesName = fnNameKeys.map((element) => {
-      if (element[1] === sq.name) element[1] = "sq";
-      if (element[1] === moreInfo.name) element[1] = "moreInfo";
-
-      return element[1];
-   });
-
+   useEffect(() => {
+      let fnNameKeys = Object.entries(functionsName);
    
-   // here, the code find the page with the useParams and the variables
-   // First find the name of the page
-   let currentPageName = pagesName.find((element) => element === cvId);
-   // Then find the name of the function
-   let currentPageFn = fnNameKeys.find(
-      (element) => element[1] === currentPageName
-   );
-   // and finally, the find the component
-   let currentPage = PAGES.find((element) => element.name === currentPageFn[0]);
+      let pagesName = fnNameKeys.map((element) => {
+         if (element[1] === sq.name) element[1] = "sq";
+         if (element[1] === moreInfo.name) element[1] = "moreInfo";
+   
+         return element[1];
+      });
+      
+      // here, the code find the page with the useParams and the variables
+      // First find the name of the page
+      let currentPageName = pagesName.find((element) => element === cvId);
+      // Then find the name of the function
+      let currentPageFn = fnNameKeys.find(
+         (element) => element[1] === currentPageName
+      );
+      // and finally, the find the component
+      setCurrentPage(PAGES.find((element) => element.name === currentPageFn[0]));
+   
+   }, [cvId]);
 
-   let navbarProps = {
-      pageName: currentPageFn,
-      pagesName: fnNameKeys
-   }
+   // let navbarProps = {
+   //    pageName: currentPageFn,
+   //    pagesName: fnNameKeys
+   // }
    return (
       <>
          {/* <Navbar {...navbarProps} /> */}
-         {currentPage()}
+         {currentPage}
       </>
    );
 }
