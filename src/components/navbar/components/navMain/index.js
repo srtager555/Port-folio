fc17881b8b8461b7eda6b876ec6e31fd1a80501f
Carlos && Ponce <srtager555@gmail.com>
2022-06-11 19:03:28 -0600

    issue in the css solved

--- src/components/navbar/components/navMain/index.js
+++ src/components/navbar/components/navMain/index.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import "../../../../styles/Navbar.css";
 
 export function NavMain({ handleResetScroll }) {
+   const [isActived, setIsActived] = useState(false);
+
+   
+   function handleScroll() {
+      let scrollY = window.scrollY;
+
+      if (scrollY > window.innerHeight * 0.1) {
+         setIsActived(true);
+      } else {
+         setIsActived(false);
+      }
+   }
+
+   useEffect(() => {
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+         window.removeEventListener("scroll", handleScroll);
+      };
+   }, []);
+
    return (
       <>
-         {/* <div className="nav__background__container"> */}
-            <div className="nav__background--main"></div>
-            <div className="nav__background--main secondary"></div>
-         {/* </div> */}
-         {/* <nav className="nav__container"> */}
-            <nav className="nav__container--main">
-               <div className="nav__logo">
-                  <Link onClick={handleResetScroll} to="/">
-                     &&
-                  </Link>
-               </div>
-
-            </nav>
-            <nav className="nav__container--cta">
-               <a href="mailto:srtager555@gmail.com">let's talk!</a>
-            </nav>
-         {/* </nav> */}
+         <div className={`nav__background--main${isActived? " show" : ""}`}></div>
+         <div className={`nav__background--main secondary${isActived? " show" : ""}`}></div>
+         <nav className={`nav__container--main${isActived? " show" : ""}`}>
+            <div className="nav__logo">
+               <Link onClick={handleResetScroll} to="/">
+                  &&
+               </Link>
+            </div>
+         </nav>
+         <nav className={`nav__container--cta${isActived? " show" : ""}`}>
+            <a href="mailto:srtager555@gmail.com">let's talk!</a>
+         </nav>
       </>
    );
 }
