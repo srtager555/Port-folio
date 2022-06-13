51cc2feee7cbf9729e510224cdedc7080e0e8293
Carlos && Ponce <srtager555@gmail.com>
2022-06-13 10:50:58 -0600

    Idk about this issue

--- src/pages/cv/index.js
+++ src/pages/cv/index.js
@@ -3,9 +3,8 @@ import React, { useEffect } from 'react';
 import { cvtemplate } from '../../template/cv/';
 
 export function CV({ setLoader, chagePath }) {
-    chagePath('cv');
-
     useEffect(() => {
+        chagePath('cv');
         setLoader(true);
     }, []);
 
