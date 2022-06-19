d71db5b01d7f81d7f1d40549d289d5357bc2627f
Carlos && Ponce <srtager555@gmail.com>
2022-06-18 21:09:24 -0600

    global animations

---
+++ src/animations/defaultAnimation/upLetter.js
@@ -0,0 +1,22 @@
+import anime from "animejs";
+
+export default function moveUpLetterAnimation(element) {
+    anime
+       .timeline({
+          duration: 500,
+          easing: "easeInOutSine",
+       })
+       .add({
+          targets: element.current.children[0].childNodes,
+          translateY: [0, "-200%"],
+          delay: anime.stagger(20),
+       })
+       .add(
+          {
+             targets: element.current.children[1].childNodes,
+             translateY: ["200%", 0],
+             delay: anime.stagger(20),
+          },
+          0
+       );
+ }
