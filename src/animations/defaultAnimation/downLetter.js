1159c4b6d71c1c50548c6559ee7ab7dd634a5f90
Carlos && Ponce <srtager555@gmail.com>
2022-06-29 16:58:14 -0600

    update the animation from the letters in the home

--- src/animations/defaultAnimation/downLetter.js
+++ src/animations/defaultAnimation/downLetter.js
@@ -9,12 +9,16 @@ export default function moveDownLetterAnimation(element, callback) {
       .add({
          targets: element[0].childNodes,
          translateY: ["100%", 0],
+         translateX: ["-20px", 0],
+         rotate: ['15deg', '0deg'],
          delay: anime.stagger(20),
       })
       .add(
          {
             targets: element[1].childNodes,
             translateY: [0, "-100%"],
+            translateX: [0, "-20px"],
+            rotate: ['0deg', '15deg'],
             delay: anime.stagger(20),
             complete: function (anim) {
                if (anim.completed) callback();
--- src/animations/defaultAnimation/upLetter.js
+++ src/animations/defaultAnimation/upLetter.js
@@ -13,12 +13,16 @@ export default function moveUpLetterAnimation(
       .add({
          targets: element[0].childNodes,
          translateY: [0, "100%"],
+         translateX: [0, "-20px"],
+         rotate: ['0deg', '15deg'],
          delay: anime.stagger(20),
       })
       .add(
          {
             targets: element[1].childNodes,
             translateY: ["-100%", 0],
+            translateX: ["-20px", 0],
+            rotate: ['15deg', '0deg'],
             delay: anime.stagger(20),
          },
          0
