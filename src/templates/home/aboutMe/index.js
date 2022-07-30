775923d20e38d0ef87c32f6118d33fa1d4dd9b21
Carlos && Ponce <srtager555@gmail.com>
2022-07-30 01:54:52 -0600

    I removed React in the imports

--- src/templates/home/aboutMe/index.js
+++ src/templates/home/aboutMe/index.js
@@ -1,4 +1,3 @@
-import React from "react";
 import WrapppingLetters from "wrapping-letters-react";
 
 export function AboutMeContainer({ l }) {
--- src/templates/home/lestTalk/index.js
+++ src/templates/home/lestTalk/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import WrappingLetters from "wrapping-letters-react";
 import anime from "animejs";
 
