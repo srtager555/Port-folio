28202042239381da001b7581989dec25c73c1293
Carlos && Ponce <srtager555@gmail.com>
2022-07-30 15:31:20 -0600

    I updated this styles to sass

--- src/components/navbar/components/navMain/index.js
+++ src/components/navbar/components/navMain/index.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
-import "../../../../styles/Navbar.css";
+import "@sass/Navbar.module.sass";
 
 export function NavMain({ handleResetScroll }) {
   const [isActived, setIsActived] = useState(false);
