f246b5c99d0abe5c9bc9b95263e994a08e8e00be
Carlos && Ponce <srtager555@gmail.com>
2022-07-30 01:47:22 -0600

    Prettier added

--- src/animations/defaultAnimation/upLetter.js
+++ src/animations/defaultAnimation/upLetter.js
@@ -1,30 +1,30 @@
 import anime from "animejs";
 
 export default function moveUpLetterAnimation(
-   element,
-   callback = function () {}
+  element,
+  callback = function () {}
 ) {
-   callback()
-   anime
-      .timeline({
-         duration: 500,
-         easing: "easeInOutSine",
-      })
-      .add({
-         targets: element[0].childNodes,
-         translateY: [0, "100%"],
-         translateX: [0, "-20px"],
-         rotate: ['0deg', '15deg'],
-         delay: anime.stagger(20),
-      })
-      .add(
-         {
-            targets: element[1].childNodes,
-            translateY: ["-100%", 0],
-            translateX: ["-20px", 0],
-            rotate: ['15deg', '0deg'],
-            delay: anime.stagger(20),
-         },
-         0
-      );
+  callback();
+  anime
+    .timeline({
+      duration: 500,
+      easing: "easeInOutSine",
+    })
+    .add({
+      targets: element[0].childNodes,
+      translateY: [0, "100%"],
+      translateX: [0, "-20px"],
+      rotate: ["0deg", "15deg"],
+      delay: anime.stagger(20),
+    })
+    .add(
+      {
+        targets: element[1].childNodes,
+        translateY: ["-100%", 0],
+        translateX: ["-20px", 0],
+        rotate: ["15deg", "0deg"],
+        delay: anime.stagger(20),
+      },
+      0
+    );
 }
