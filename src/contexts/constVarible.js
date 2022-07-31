7d13b906b3eac7bbbf8c22a93f3e92b2cada7287
Carlos && Ponce <srtager555@gmail.com>
2022-07-30 19:50:13 -0600

    Mmm... for now it's repaired

--- src/contexts/constVarible.js
+++ src/contexts/constVarible.js
@@ -1,27 +1,29 @@
-const MOBILE_SIZE = 610;
 // const TABLET_SIZE = 1024;
+const MOBILE_SIZE = 610;
+export const NAVBAR_MOBILE_SIZE = 510;
 const MOBILE_DEVICE_SIZE = 1024;
 
-export const NAVBAR_MOBILE_SIZE = 510;
-
 let IS_MOBILE__ = false;
+let IS_MOBILE_DEVICE__ = false;
+function useConstSizes() {
+  window.addEventListener("resize", () => IS_MOBILE());
 
-window.addEventListener("resize", () => IS_MOBILE());
-
-function IS_MOBILE() {
-  IS_MOBILE__ = window.innerWidth < MOBILE_DEVICE_SIZE;
-
-  return IS_MOBILE__;
+  window.addEventListener("resize", () => IS_MOBILE_DEVICE(IS_MOBILE_DEVICE__));
 }
 
-let IS_MOBILE_DEVICE__ = false;
-
-window.addEventListener("resize", () => IS_MOBILE_DEVICE(IS_MOBILE_DEVICE__));
-
-function IS_MOBILE_DEVICE() {
+useConstSizes.prototype.IS_MOBILE_DEVICE = function () {
   IS_MOBILE_DEVICE__ = window.innerWidth < MOBILE_SIZE;
 
   return IS_MOBILE_DEVICE__;
-}
+};
+
+useConstSizes.prototype.IS_MOBILE = function () {
+  IS_MOBILE__ = window.innerWidth < MOBILE_DEVICE_SIZE;
+
+  return IS_MOBILE__;
+};
+
+const IS_MOBILE = useConstSizes.IS_MOBILE;
+const IS_MOBILE_DEVICE = useConstSizes.IS_MOBILE_DEVICE;
 
 export { IS_MOBILE, IS_MOBILE_DEVICE };
