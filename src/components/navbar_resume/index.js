b442bc8bf2b9a2beb0f5d00055932b11df6f3caa
Carlos && Ponce <srtager555@gmail.com>
2022-08-14 14:05:34 -0600

    I'm upgrading the navbar from resume

--- src/components/navbar_resume/index.js
+++ src/components/navbar_resume/index.js
@@ -1,67 +1,39 @@
 import { useState, useEffect } from "react";
-import { Link } from "next/link";
+import Link from "next/link";
 
 // import { MenuButton } from "./menuButton";
 
 import styles from "@sass/resumeStyles/navbar.module.sass";
 
-export function Navbar({ pageName, setPageName, pagesName }) {
-  const [showButton, setShowButton] = useState(true);
-  const [buttons, setButtons] = useState([]);
-  const pageIndex = pagesName.indexOf(pageName);
+export function Navbar({ currentPage, pagesNameList }) {
+  const [pageIndex, setPageIndex] = useState(
+    pagesNameList.indexOf(currentPage)
+  );
+  const [previusPage, setPreviusPage] = useState(
+    pagesNameList[pageIndex - 1] || pagesNameList[pagesNameList.length - 1]
+  );
+  const [nextPage, setNextPage] = useState(pagesNameList[pageIndex + 1]);
 
-  // const meuButtonProps = {
-  //    pageName,
-  //    handleChangePage,
-  //    pagesName,
-  // };
+  useEffect(() => {
+    setPageIndex(pagesNameList.indexOf(currentPage));
 
-  function HandleRouteName(text) {
-    let path = text;
-    if (text === "s&q") path = "sq";
-
-    return path;
-  }
-
-  function linkComponent(text) {
-    return (
-      <Link href={`/resume/${HandleRouteName(text)}`}>
-        <a>{text}</a>
-      </Link>
+    setPreviusPage(
+      pagesNameList[pageIndex - 1] || pagesNameList[pagesNameList.length - 1]
     );
-  }
-
-  useEffect(() => {
-    //here change the name of a string on the arr
-    let a = pagesName.find((e) => e[1] === "sq");
-    a[1] = "s&q";
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pageName]);
-
-  useEffect(() => {
-    //here change the sections to go, depending on the pageName
-    let link = (prop) => linkComponent(pagesName[prop][1]);
-
-    if (pageIndex === 0) {
-      setButtons([link(pagesName.length - 1), link(pageIndex + 1)]);
-    } else if (pageIndex === pagesName.length - 1) {
-      setButtons([link(pageIndex - 1), link(0)]);
-    } else {
-      setButtons([link(pageIndex - 1), link(pageIndex + 1)]);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pageIndex]);
+    setNextPage(pagesNameList[pageIndex + 1]);
+  }, [currentPage]);
 
   return (
     <>
-      {/* <MenuButton {...meuButtonProps} /> */}
-      <div className={styles.container}>
-        {/* here only print the arr of links */}
-        {showButton &&
-          buttons.map((element, index) => (
-            <span key={`button-${index}`}>{element}</span>
-          ))}
-      </div>
+      <nav className={styles.container}>
+        <div className={styles.menu}>{}</div>
+        <Link href={`/resume/${previusPage}`}>
+          <a>{previusPage}</a>
+        </Link>
+        <Link href={`/resume/${nextPage}`}>
+          <a>{nextPage}</a>
+        </Link>
+      </nav>
     </>
   );
 }
