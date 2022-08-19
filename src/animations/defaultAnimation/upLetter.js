3a5f278cad563080df9a1f6cb2957f400400f7ed
Carlos && Ponce <srtager555@gmail.com>
2022-08-18 18:37:36 -0600

    I changed animejs for vanilla js

--- src/animations/defaultAnimation/upLetter.js
+++ src/animations/defaultAnimation/upLetter.js
@@ -1,30 +1,15 @@
-import anime from "animejs";
-
 export default function moveUpLetterAnimation(
   element,
   callback = function () {}
 ) {
   callback();
-  anime
-    .timeline({
-      duration: 500,
-      easing: "easeInOutSine",
-    })
-    .add({
-      targets: element[0].childNodes,
-      translateY: [0, "100%"],
-      translateX: [0, "-20px"],
-      rotate: ["0deg", "15deg"],
-      delay: anime.stagger(20),
-    })
-    .add(
-      {
-        targets: element[1].childNodes,
-        translateY: ["-100%", 0],
-        translateX: ["-20px", 0],
-        rotate: ["15deg", "0deg"],
-        delay: anime.stagger(20),
-      },
-      0
-    );
+
+  element[0].childNodes.forEach((target, index) => {
+    target.style.transition = `all 500ms ease ${20 * index}ms`;
+    target.style.transform = `translateY(100%) translateX(-20px) rotate(15deg)`;
+  });
+  element[1].childNodes.forEach((target, index) => {
+    target.style.transition = `all 500ms ease ${20 * index}ms`;
+    target.style.transform = `translateY(0%) translateX(0px) rotate(0deg)`;
+  });
 }
