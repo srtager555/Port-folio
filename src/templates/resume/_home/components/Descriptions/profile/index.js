4fa491949db39ec65fada4928292bd028b849d8a
Carlos && Ponce <srtager555@gmail.com>
2022-08-26 17:09:07 -0600

    I added wrapping-letters for the styles

--- src/templates/resume/_home/components/Descriptions/profile/index.js
+++ src/templates/resume/_home/components/Descriptions/profile/index.js
@@ -1,9 +1,19 @@
 import Wl from "wrapping-letters-react";
 
+import styles from "@sass/resumeStyles/home/descriptionStyles.module.sass";
+
 export function ProfileDescription() {
   return (
     <p>
-      <Wl text="A brief explanation of who I am, what I do and how I do it." />
+      <Wl
+        text="A brief explanation of who I am, what I do and how I do it."
+        textOptions={{
+          SelectClass: {
+            wordToSearch: ["what", "who", "I", "am", "do", "how", "it"],
+            classToAdd: styles["italic-letters"],
+          },
+        }}
+      />
     </p>
   );
 }
