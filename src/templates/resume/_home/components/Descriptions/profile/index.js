f796e22df9ce4494ca6cf39598e016ab370a2642
Carlos && Ponce <srtager555@gmail.com>
2022-08-25 22:09:02 -0600

    I need start the design of the description...

--- src/templates/resume/_home/components/Descriptions/profile/index.js
+++ src/templates/resume/_home/components/Descriptions/profile/index.js
@@ -1,5 +1,9 @@
 import Wl from "wrapping-letters-react";
 
 export function ProfileDescription() {
-  return <p>A brief explanation of who I am, what I do and how I do it.</p>;
+  return (
+    <p>
+      <Wl text="A brief explanation of who I am, what I do and how I do it." />
+    </p>
+  );
 }
