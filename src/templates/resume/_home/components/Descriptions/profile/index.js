bfc63c37419f054500e78a9789fa0c80fdd5dfff
Carlos && Ponce <srtager555@gmail.com>
2022-08-26 18:50:53 -0600

    Issue solved in the animation and in the iteration

--- src/templates/resume/_home/components/Descriptions/profile/index.js
+++ src/templates/resume/_home/components/Descriptions/profile/index.js
@@ -1,17 +1,40 @@
+import { useState, useEffect } from "react";
+
 import Wl from "wrapping-letters-react";
 
 import styles from "@sass/resumeStyles/home/descriptionStyles.module.sass";
 
+function SpecialTag({ letter }) {
+  const [animation, setAnimation] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setAnimation(true), 100);
+  }, []);
+  return (
+    <div
+      className={`${styles["inline-container"]} ${styles["down-animation"]}${
+        animation ? ` ${styles["normal-position"]}` : ""
+      }`}
+      style={{ "--i": 1 }}
+    >
+      <span className={styles["italic-letters"]}>{letter}</span>
+    </div>
+  );
+}
+
 export function ProfileDescription() {
+  const words = ["what", "who", "I", "am,", "do", "how", "it."];
+
   return (
     <p>
       <Wl
         text="A brief explanation of who I am, what I do and how I do it."
         textOptions={{
-          SelectClass: {
-            wordToSearch: ["what", "who", "I", "am", "do", "how", "it"],
-            classToAdd: styles["italic-letters"],
+          SpecialWrapp: {
+            wordToSearch: words,
+            structureToAdd: SpecialTag,
           },
+          PerWord: true,
         }}
       />
     </p>
