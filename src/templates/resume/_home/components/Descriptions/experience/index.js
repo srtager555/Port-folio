37a88751169c2925edc5d11b3cc7f0724f20853a
Carlos && Ponce <srtager555@gmail.com>
2022-09-04 02:53:45 -0600

    I started the styles for experience description

--- src/templates/resume/_home/components/Descriptions/experience/index.js
+++ src/templates/resume/_home/components/Descriptions/experience/index.js
@@ -1,6 +1,6 @@
 import Wl from "wrapping-letters-react";
 
-import styles from "@sass/resumeStyles/home/descriptionStyles.module.sass"
+import styles from "@sass/resumeStyles/home/descriptionStyles.module.sass";
 
 export function ExperienceDescription() {
   return (
@@ -8,13 +8,13 @@ export function ExperienceDescription() {
       <Wl
         text="I've been working on a few projects that give me some experience on
       this topic, check them out."
-
-      textOptions={{
-        SelectClass: {
-          wordToSearch: "projects"
-          classToAdd: styles[""]
-        }
-      }}
+        textOptions={{
+          SelectClass: {
+            wordToSearch: ["experience on this topic", "projects"],
+            classToAdd: styles["italic-letters"],
+          },
+          PerWord: false,
+        }}
       />
     </p>
   );
