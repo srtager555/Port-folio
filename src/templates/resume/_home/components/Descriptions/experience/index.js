f9b18ebceb4e81ed6286a86d0462bfa29b7f9785
Carlos && Ponce <srtager555@gmail.com>
2022-09-05 00:05:01 -0600

    new animation added on the description

--- src/templates/resume/_home/components/Descriptions/experience/index.js
+++ src/templates/resume/_home/components/Descriptions/experience/index.js
@@ -2,6 +2,26 @@ import Wl from "wrapping-letters-react";
 
 import styles from "@sass/resumeStyles/home/descriptionStyles.module.sass";
 
+function letter({ letter, cssClass, specialWrapp, index }) {
+  const { hasCustomWrapp, NewWrappStructure } = specialWrapp;
+
+  return hasCustomWrapp ? (
+    <NewWrappStructure letter={letter} index={index} />
+  ) : (
+    <span className={cssClass}>{letter}</span>
+  );
+}
+
+function CustomTag({ letter, index }) {
+  let customStyles = { animationDelay: `${index * 100 - 5400}ms` };
+
+  return (
+    <span className={styles["levelUp-animation"]} style={customStyles}>
+      {letter}
+    </span>
+  );
+}
+
 export function ExperienceDescription() {
   return (
     <p>
@@ -10,11 +30,16 @@ export function ExperienceDescription() {
       this topic, check them out."
         textOptions={{
           SelectClass: {
-            wordToSearch: ["experience on this topic", "projects"],
+            wordToSearch: "projects",
             classToAdd: styles["italic-letters"],
           },
-          PerWord: false,
+
+          SpecialWrapp: {
+            wordToSearch: "experience on this topic",
+            structureToAdd: CustomTag,
+          },
         }}
+        structure={letter}
       />
     </p>
   );
